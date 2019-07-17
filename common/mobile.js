0d5dbf28d767578c8242be90a5aab3f6ed7f1e15
yy <1023767856@qq.com>
2019-07-17 14:54:30 +0800

    update .gitignore

--- common/mobile.js
+++ common/mobile.js
@@ -7,7 +7,7 @@ const timeout = 60 * 1000;
 
 const proxy = {
    host: "127.0.0.1",
-    port: info.proxy.split(":")[1] 
+    port: info.proxy.split(":")[1]
   }
 
 const config = {
@@ -18,6 +18,9 @@ const config = {
     TZ: info.timezone,
     ...process.env
   },
+  //  incognito => Causes the browser to launch directly in incognito mode.
+  //  proxy-server => Uses a specified proxy server, overrides system settings. This switch only affects HTTP and HTTPS requests.
+  //  lang => 	The language file that we want to try to open. Of the form language[-country] where language is the 2 letter code from ISO-639
   args: ["--incognito", `--proxy-server=${info.proxy}`, `--lang=${info.lang}`]
 }
 
